7e4e3531dce495457354ed111aa41266b5ebcdbb
Travis <travis.x.wong@gmail.com>
2020-03-04 08:42:05 +0800

    update

---
+++ conditional_tests.py
@@ -0,0 +1,19 @@
+name = 'karate'
+print("Is name == 'karate'? I predict True.")
+print(name == 'karate')
+
+print("\nIs name == 'Karate'? I predict False.")
+print(name == 'Karate')
+
+name = 'Muddy Puddle'
+print("\nIs name.lower() == 'muddy puddle'? I predict True")
+print(name.lower() == 'muddy puddle')
+
+print(1 > 2)
+print(2 <= 2)
+print(3 != 4)
+
+cars = ['bmw', 'bens', 'truck', 'wagon']
+print('bmw' in cars)
+print('sss' in cars)
+print('ddd' not in cars)
