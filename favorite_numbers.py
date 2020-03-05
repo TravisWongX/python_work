34b57f8563528bce8672e5e07ec89631fb3517b6
Travis <travis.x.wong@gmail.com>
2020-03-05 10:39:31 +0800

    case6

---
+++ favorite_numbers.py
@@ -0,0 +1,13 @@
+favorite_numbers = {
+    'eric': 20,
+    'jessy': 36,
+    'cholli': 14,
+    'mei': 27,
+    'andy': 3,
+    }
+
+print("eric: " + str(favorite_numbers['eric']))
+print("jessy: " + str(favorite_numbers['jessy']))
+print("cholli: " + str(favorite_numbers['cholli']))
+print("mei: " + str(favorite_numbers['mei']))
+print("andy: " + str(favorite_numbers['andy']))
---
+++ person.py
@@ -0,0 +1,7 @@
+person = {
+    'name': 'polly', 
+    'age': 18,
+    'gender': 'male', 
+    'address': 'New York'
+    }
+print(person)
